28e080c355a7e38bb4c6168a961ea4fb8b8555cc
` oSunN 疼 ! <aleatorioif@gmail.com>
2026-03-13 20:59:10 -0300

    Update usuarios.php

--- usuarios.php
+++ usuarios.php
@@ -8,7 +8,6 @@
     <link rel="stylesheet" href="dash.css">
 </head>
 <body>
-    sasasa
 
     <div class="dashboard-container"> <nav class="sidebar"> <ul>
                 <li><a href="index.php"><i class="fa-solid fa-house"></i> Início</a></li>
